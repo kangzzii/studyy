09deddffd9e62d75707970cee698c1ad2ef75933
kangzzii <rkd3116@naver.com>
2025-04-08 17:59:23 +0900

    Reapply "이거 올라가나?!"
    This reverts commit a5a77ac5877a21a4cef4b806f0af3371a0d55f3a.

--- solCademy/src/main/java/egovframework/example/calendar/controller/CalendarController.java
+++ solCademy/src/main/java/egovframework/example/calendar/controller/CalendarController.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import egovframework.example.calendar.service.CalendarService;
 
 
 @Controller
