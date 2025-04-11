9cf268f254f61fd8bb441b4e5ba13d14dd6b82b7
kangzzii <rkd3116@naver.com>
2025-04-11 11:47:17 +0900

    no message

--- solCademy/src/main/java/egovframework/example/sample/web/TilesTest.java
+++ solCademy/src/main/java/egovframework/example/sample/web/TilesTest.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TilesTest {
-	
+
 	@RequestMapping(value="/calendar.do")
 	public String doCalendar() {
 		return "tiles/calendar/main";
 	}
-	
+
 	@RequestMapping(value="/tiles/popup.do")
 	public String PopTiles() {
 		return "popup/test/test";
